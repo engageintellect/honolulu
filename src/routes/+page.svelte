276274e04b2cc80122c7af846cad8df4874b63dc
engageintellect <jc9361@gmail.com>
2023-11-25 04:24:14 -0800

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <!-- HERO -->
 
-<div class=" h-screen">
-	<div class="h-2/3 sticky w-full bg-gradient-to-t from-zinc-900 to-purple-500">
+<div class="">
+	<div class="h-screen sticky w-full bg-gradient-to-t from-zinc-900 to-purple-500">
 		<img
 			class="h-full w-full object-cover opacity-50 relative"
 			src="https://images.unsplash.com/photo-1700159915754-2b4ba5b2e3be?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -11,7 +11,7 @@
 		<div class="absolute top-0 right-0 w-full z-50">
 			<div class="max-w-7xl mx-auto text-right p-2">
 				<button
-					class="bg-white px-4 py-2 rounded uppercase sm:hover:bg-fuchsia-700 sm:hover:border-fuchsia-700 sm:hover:text-white border border-white sm:hover:scale-105 transition-scale duration-300"
+					class="bg-white px-4 py-2 rounded uppercase sm:hover:bg-fuchsia-700 sm:hover:border-fuchsia-700 sm:hover:text-white border border-white sm:hover:scale-[102%] transition-scale duration-300"
 					>Contact</button
 				>
 			</div>
@@ -19,53 +19,36 @@
 
 		<div class="absolute top-0 flex items-center w-full h-full">
 			<div class="p-6 flex flex-col gap-5 max-w-7xl mx-auto justify-center w-full h-full">
-				<div class="text-5xl lg:text-7xl text-white max-w-xl box-shadow">Explore new worlds.✈️</div>
+				<div
+					class="text-5xl lg:text-7xl text-white max-w-xl font-semibold drop-shadow drop-red-300"
+				>
+					Explore new worlds.✈️
+				</div>
 				<div class="text-lg md:text-2xl text-white max-w-lg box-shadow">
 					This is a tagline that should say something interesting, and be one to two sentances long.
+					Or longer, if you want.
 				</div>
 				<div>
 					<div class="flex gap-2">
 						<button
-							class="bg-white px-4 py-2 rounded uppercase sm:hover:bg-fuchsia-700 sm:hover:border-fuchsia-700 sm:hover:text-white border border-white sm:hover:scale-105 transition-scale duration-300"
+							class="bg-white px-4 py-2 rounded uppercase sm:hover:bg-fuchsia-700 sm:hover:border-fuchsia-700 sm:hover:text-white border border-white sm:hover:scale-[102%] transition-scale duration-300"
 							>Get Started</button
 						>
-						<button
-							class="border border-white text-white px-4 py-2 rounded uppercase sm:hover:scale-105 transition-scale duration-300"
-							>Learn More</button
-						>
+
+						<a href="#heading">
+							<button
+								class="border border-white text-white px-4 py-2 rounded uppercase sm:hover:scale-[102%] transition-scale duration-300"
+								>Learn More</button
+							>
+						</a>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<!-- AI -->
-	<div class="h-1/2 relative">
-		<div class="absolute top-0 bg-gradient-to-b from-fuchsia-900 to-fuchsia-200 w-full h-full">
-			<div class="flex flex-col h-full justify-center items-center">
-				<div class="text-5xl lg:text-7xl text-white max-w-xl box-shadow -z-[-1]">
-					AI Powered by OpenAI.
-				</div>
-			</div>
-		</div>
-
-		<div class="flex flex-col sm:flex-row h-full">
-			<img
-				class="h-full w-full object-cover opacity-50"
-				src="https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-				alt=""
-			/>
-
-			<img
-				class="h-full w-full object-cover opacity-50"
-				src="https://www.reuters.com/resizer/HNQC8JRbdgRbGVSg7PZUWZaCYBI=/1080x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/AZ3C2MC6TFISFPLH23LDWY2B74.jpg"
-				alt=""
-			/>
-		</div>
-	</div>
-
 	<!-- HEADING -->
-	<div class="min-h-1/2 bg-gradient-to-b from-zinc-300 to-zinc-200 py-10 sticky top-0">
+	<div id="heading" class="min-h-1/2 bg-gradient-to-b from-zinc-300 to-zinc-200 py-10 sticky top-0">
 		<div class="flex flex-col justify-center p-6 max-w-xl lg:max-w-5xl mx-auto drop-shadow">
 			<h1 class="text-3xl lg:text-5xl pb-5 text-white">
 				Aliquip aliqua minim elit minim consectetur incididunt
@@ -80,9 +63,67 @@
 		</div>
 	</div>
 
+	<div class="">
+		<!-- AI -->
+		<div class="grid grid-cols-1 sm:grid-cols-2">
+			<div class="relative bg-zinc-900 h-full w-full flex">
+				<img
+					class="h-full w-full object-cover opacity-50"
+					src="https://images.pexels.com/photos/6153354/pexels-photo-6153354.jpeg"
+					alt=""
+				/>
+				<div
+					class="absolute top-0 text-white p-2 text-3xl w-full h-full flex justify-center items-center"
+				>
+					AI-powered travel companion.
+				</div>
+			</div>
+
+			<div class="relative bg-zinc-900 h-full w-full flex">
+				<img
+					class="h-full w-full object-cover opacity-50"
+					src="https://images.pexels.com/photos/249581/pexels-photo-249581.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+					alt=""
+				/>
+				<div
+					class="absolute top-0 text-white p-2 text-3xl w-full h-full flex justify-center items-center"
+				>
+					Schedules flights for you.
+				</div>
+			</div>
+
+			<!-- AI -->
+			<div class="relative bg-zinc-900 h-full w-full flex">
+				<img
+					class="h-full w-full object-cover opacity-50"
+					src="https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg"
+					alt=""
+				/>
+				<div
+					class="absolute top-0 text-white p-2 text-3xl w-full h-full flex justify-center items-center"
+				>
+					Books your hotel or AirBnB.
+				</div>
+			</div>
+
+			<div class="relative bg-zinc-900 h-full w-full flex">
+				<img
+					class="h-full w-full object-cover opacity-50"
+					src="https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+					alt=""
+				/>
+				<div
+					class="absolute top-0 text-white p-2 text-3xl w-full h-full flex justify-center items-center"
+				>
+					A single connection to everything.
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<!-- CARDS -->
 
-	<div class="bg-zinc-200 min-h-1/2">
+	<div class="bg-zinc-200 min-h-1/2 sticky">
 		<div
 			class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 h-full w-full p-2 gap-2 lg:max-w-5xl mx-auto py-10"
 		>
@@ -153,6 +194,68 @@
 					src="https://images.unsplash.com/photo-1532581140115-3e355d1ed1de?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 					alt=""
 				/>
+			</div>
+		</div>
+	</div>
+
+	<!-- CARDS -->
+
+	<div class="bg-zinc-200 min-h-1/2 sticky top-0">
+		<div
+			class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 h-full w-full p-2 gap-2 lg:max-w-5xl mx-auto py-10"
+		>
+			<div
+				class="group cursor-pointer bg-base shadow h-full w-full rounded flex flex-col justify-start gap-2 relative bg-zinc-900"
+			>
+				<div class=" text-white p-6 -z-[-1]">
+					<div class="relative flex flex-col gap-2">
+						<div class="text-3xl">Free</div>
+						<div class="">
+							Culpa qui incididunt ad commodo cupidatat sunt eu. Sit ex officia aliqua qui ad Culpa
+							qui incididunt ad commodo cupidatat sunt eu. Sit ex officia aliqua qui ad Culpa qui
+						</div>
+						<button
+							class="border border-white backdrop-blur hover:bg-fuchsia-700 hover:border-fuchsia-700 active:scale-[99%] transition-colors duration-300 px-4 py-2 text-lg text-white uppercase rounded"
+							>click me</button
+						>
+					</div>
+				</div>
+			</div>
+
+			<div
+				class="group cursor-pointer bg-base shadow h-full w-full rounded flex flex-col justify-start gap-2 relative bg-zinc-900"
+			>
+				<div class=" text-white p-6 -z-[-1]">
+					<div class="relative flex flex-col gap-2">
+						<div class="text-3xl">Hotels</div>
+						<div class="">
+							Culpa qui incididunt ad commodo cupidatat sunt eu. Sit ex officia aliqua qui ad Culpa
+							qui incididunt ad commodo cupidatat sunt eu. Sit ex officia aliqua qui ad Culpa qui
+						</div>
+						<button
+							class="border border-white backdrop-blur hover:bg-fuchsia-700 hover:border-fuchsia-700 active:scale-[99%] transition-colors duration-300 px-4 py-2 text-lg text-white uppercase rounded"
+							>click me</button
+						>
+					</div>
+				</div>
+			</div>
+
+			<div
+				class="group cursor-pointer bg-base shadow h-full w-full rounded flex flex-col justify-start gap-2 relative bg-zinc-900"
+			>
+				<div class=" text-white p-6 -z-[-1]">
+					<div class="relative flex flex-col gap-2">
+						<div class="text-3xl">Vehicles</div>
+						<div class="">
+							Culpa qui incididunt ad commodo cupidatat sunt eu. Sit ex officia aliqua qui ad Culpa
+							qui incididunt ad commodo cupidatat sunt eu. Sit ex officia aliqua qui ad Culpa qui
+						</div>
+						<button
+							class="border border-white backdrop-blur hover:bg-fuchsia-700 hover:border-fuchsia-700 active:scale-[99%] transition-colors duration-300 px-4 py-2 text-lg text-white uppercase rounded"
+							>click me</button
+						>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
